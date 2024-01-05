8f4a7e9a47dcd259193b5e2d6e8f3a93e102a78a
余俊帆 <seeryujunfan@qq.com>
2024-01-05 10:42:02 +0800

    完成汉堡布局

---
+++ window/ViewModels/ShellViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using MahApps.Metro.IconPacks;
+using MahAppsMetroHamburgerMenuNavigation.Mvvm;
+using WpfApp1.window.Views;
+
+namespace WpfApp1.window.ViewModels
+{
+    public class ShellViewModel : BindableBase
+    {
+        public ObservableCollection<MenuItem> Menu { get; } =  new ObservableCollection<MenuItem>();
+
+        public ObservableCollection<MenuItem> OptionsMenu { get; } = new ObservableCollection<MenuItem>();
+
+        public ShellViewModel()
+        {
+            // Build the menus
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.BugSolid },
+                Label = "Bugs",
+                NavigationType = typeof(BugsPage),
+                NavigationDestination = new Uri("window/Views/BugsPage.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.UserSolid },
+                Label = "User",
+                NavigationType = typeof(UserPage),
+                NavigationDestination = new Uri("window/Views/UserPage.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CoffeeSolid },
+                Label = "Break",
+                NavigationType = typeof(BreakPage),
+                NavigationDestination = new Uri("window/Views/BreakPage.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.Menu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.FontAwesomeBrands },
+                Label = "Awesome",
+                NavigationType = typeof(AwesomePage),
+                NavigationDestination = new Uri("window/Views/AwesomePage.xaml", UriKind.RelativeOrAbsolute)
+            });
+
+            this.OptionsMenu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CogsSolid },
+                Label = "Settings",
+                NavigationType = typeof(SettingsPage),
+                NavigationDestination = new Uri("window/Views/SettingsPage.xaml", UriKind.RelativeOrAbsolute)
+            });
+            this.OptionsMenu.Add(new MenuItem()
+            {
+                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.InfoCircleSolid },
+                Label = "About",
+                NavigationType = typeof(AboutPage),
+                NavigationDestination = new Uri("window/Views/AboutPage.xaml", UriKind.RelativeOrAbsolute)
+            });
+        }
+    }
+}
